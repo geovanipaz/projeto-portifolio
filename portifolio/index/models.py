f5875454df25c24ad7263ddd4c8034498e08c6ce
Geovani Paz <geovanipaz7@gmail.com>
2022-03-07 10:30:04 -0300

    criação do model

--- portifolio/index/models.py
+++ portifolio/index/models.py
@@ -1,3 +1,10 @@
+from distutils.command.upload import upload
+from pyexpat import model
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+class about(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(max_length=800, blank=False)
+    image = models.ImageField(upload_to='about/', blank=False)
