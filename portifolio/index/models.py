320ab6adec8ea0dfb5ef05a10087130470792aeb
Geovani Paz <geovanipaz7@gmail.com>
2022-03-07 13:50:27 -0300

    adição de imagens

--- portifolio/index/models.py
+++ portifolio/index/models.py
@@ -8,3 +8,6 @@ class about(models.Model):
     title = models.CharField(max_length=100, blank=False)
     description = models.TextField(max_length=800, blank=False)
     image = models.ImageField(upload_to='about/', blank=False)
+
+    def __str__(self) -> str:
+        return self.title
